9fd8a125cab655200cbdbdc242b492a304cb2e84
iinnovations1 <Rammiya.Narayanasamy@axiatadigitallabs.com>
2021-05-20 16:52:43 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hiiiii Everyone</h1>
+        <h1>Hiiii Everyone</h1>
       </header>
     </div>
   );
